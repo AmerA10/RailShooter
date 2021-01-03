d0779c2e6ac812bc91dc1521e7d5ea303eccb0f8
AmerA10 <ajami.amer1@gmail.com>
2021-01-03 16:14:27 -0700

    camera movement, still need work

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 public class MusicPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
     void Start()
     {
         Invoke("LoadFirstScene", 3f);
+        
     }
 
     // Update is called once per frame
