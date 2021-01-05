d870dfb7ceff8d23af84e14543b3e5cacfe00c22
AmerA10 <ajami.amer1@gmail.com>
2021-01-04 22:09:35 -0700

    death stuff

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement; //ok to have as long as only script that loads scene on the object
 public class CollisionHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject explosion;
     void Start()
     {
         
     }
     private void OnTriggerEnter(Collider other)
     {
+        explosion.SetActive(true);
         StartDeathSequence();
+        Invoke("ReloadScene", levelLoadDelay);
+    }
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
     // Update is called once per frame
     void Update()
@@ -21,7 +30,8 @@ public class CollisionHandler : MonoBehaviour
 
    private void StartDeathSequence()
    {
-        Debug.Log("Player is dead");
-        SendMessage("OnPlayerDeath");
+        
+        SendMessage("OnPlayerDeath"); //
+        
    }
 }
