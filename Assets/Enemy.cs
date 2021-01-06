bc6c7b06404b7c3d9eea5122beee96edcfa7d1ad
AmerA10 <ajami.amer1@gmail.com>
2021-01-05 21:10:29 -0700

    creating colliders at run time

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Collider selfBoxCollider;
     void Start()
     {
-        
+
+        AddNonTriggerBoxCollider();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddNonTriggerBoxCollider()
     {
+        if(this.gameObject.GetComponent<BoxCollider>() == null)
+        {
+            selfBoxCollider = gameObject.AddComponent<BoxCollider>();
+        } 
+        else
+        {
+            selfBoxCollider = this.GetComponent<BoxCollider>();
+        }
+
         
+        selfBoxCollider.isTrigger = false;
     }
 
     private void OnParticleCollision(GameObject other)
